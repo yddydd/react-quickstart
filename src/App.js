a51b2f7db57e56ecdbed477ce165e67505c630fe
yangshi <yangshi@sensorsdata.cn>
2020-04-13 17:42:16 +0800

    feat: inject router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import FunctionComp from './components/functionComp'
@@ -7,21 +13,30 @@ import ListComp from './components/listComp'
 import FormComp from './components/formComp'
 import RefComp from './components/refComp'
 import StateHook from './components/stateHook'
+import About from './pages/About'
 
 function App() {
   const ref = React.createRef();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <FunctionComp from="app"/>
-        <ClassComp />
-        <ListComp numbers={[1, 2, 3]}/>
-        <FormComp />
-        <RefComp ref={ref} />
-        <StateHook />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+        <footer className="App-header">
+          <Link to="/about">About</Link>
+          <img src={logo} className="App-logo" alt="logo" />
+          <FunctionComp from="app"/>
+          <ClassComp />
+          <ListComp numbers={[1, 2, 3]}/>
+          <FormComp />
+          <RefComp ref={ref} />
+          <StateHook />
+        </footer>
+      </div>
+    </Router>
   );
 }
 
