39152061943dfdb5d6cff02688783207485eaf7a
yangshi <yangshi@sensorsdata.cn>
2020-04-13 13:55:56 +0800

    feat: component

---
+++ src/components/classComp.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default class comp extends React.Component {
+  render() {
+    return <h1>class组件</h1>
+  }
+}
---
+++ src/components/functionComp.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function createComponent(props) {
+  console.log('????', props)
+  return <h1>函数组件</h1>
+}
+
+export default createComponent
