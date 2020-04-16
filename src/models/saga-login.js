4e55fd6680b20dca59db78c2e3a68928a7e17820
yangshi <yangshi@sensorsdata.cn>
2020-04-16 14:30:15 +0800

    feat: saga-api

--- src/models/saga-login.js
+++ src/models/saga-login.js
@@ -1,4 +1,9 @@
-import { take, call, put } from 'redux-saga/effects'
+import { take, call, put, fork, cancel, cancelled } from 'redux-saga/effects'
+/*
+ * fork(fn, params) 参数和call一样 另起线程执行 无阻塞调用 
+ * cancel(task) 参数为fork的返回值 取消掉正在进行的任务
+ * take([ACTION.type,...]) 监听两个并发的ACTION 匹配其一就执行
+ */
 
 function* authorize(payload) {
   try {
@@ -11,6 +16,10 @@ function* authorize(payload) {
     return token
   } catch(err) {
     put({type: 'LOGIN_ERROR', err})
+  } finally {
+    if (yield cancelled()) {
+      console.log('oops i was canceled')
+    }
   }
 }
 
@@ -20,10 +29,27 @@ function* saga() {
     const action = yield take('LOGIN_REQUEST')
     const { username, password } = action.payload
     const token = yield call(authorize, { username, password })
+    // authorize没验证完之前 收到了LOGOUT怎么办？ 登出后 又突然登陆成功 岂不是有问题
     if(token) {
       console.log('===存写===', token)
     }
   }
 }
 
-export default saga
+//改造
+
+function* saga2() {
+  while(true) {
+    const action = yield take('LOGIN_REQUEST')
+    const { username, password } = action.payload
+
+    const task = yield fork(authorize, {username, password})
+    const block = yield take(['LOGOUT', 'LOGIN_ERROR'])
+    if (block.type === 'LOGOUT') {
+      yield cancel(task)
+    }
+    // 
+  }
+}
+
+export default saga2
